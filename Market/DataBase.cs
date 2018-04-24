91663cae911faa19124ebea1ec9d8da30bbfa98f
Sergey Nikitin <nikitinserv@gmail.com>
2018-04-24 22:07:25 +0400

    start

---
+++ Market/DataBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market
+{
+    class DataBase
+    {
+        static SQLiteConnection getConnection()
+        {
+            return new SQLiteConnection("Data Source=filename.db; Version=3;");
+        }
+
+        static DataBase()
+        {
+            using (SQLiteConnection conn = getConnection())
+            {
+                runEmpty("CREATE TABLE IF NOT EXISTS accounts("
+             + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+             + "name TEXT NOT NULL, "
+             + "password_hash TEXT NOT NULL, "
+             + "isRoot BOOLEAN NOT NULL;");
+            }
+
+            //DataSet data = new System.Data.DataSet();
+            //data.Reset();
+            //SQLiteDataAdapter ad = new SQLiteDataAdapter(createComand("SELECT * from accounts WHERE name='root';"));
+            //ad.Fill(data);
+
+        }
+
+       public static bool checkUserExists(String userName, String password)
+        {
+
+            return true;
+
+        }
+
+        private static void runEmpty(String text)
+        {
+            using (SQLiteConnection conn = getConnection())
+            {
+                SQLiteCommand cmd = createComand(text, conn);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SQLiteException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static SQLiteCommand createComand(String text, SQLiteConnection conn)
+        {
+            SQLiteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = text;
+            return cmd;
+        }
+    }
+}
---
+++ Market/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Market
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataBase.checkUserExists("123", "23");
+
+        }
+
+    }
+}
